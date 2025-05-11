bf4bb5359556e9c8d9f4606c863cca8db71326d5
K14aNB <kirnb@protonmail.com>
2025-05-11 20:46:05 +0530

    Added Merge Sort Algorithm

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,13 @@
+"""
+Merge Sort Algorithm
+"""
+
+def merge(arr:list[int], left:int, mid:int, right:int) -> None:
+    """
+    Function to merge the lists.
+
+    Arguments
+    ---------
+        arr (list[int]): List of integers
+        left (int): lef
+    """
